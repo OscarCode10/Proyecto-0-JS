3d7b78cb43a4199a3740fca1db832df6d0b84385
unknown <125331289+OscarCode10@users.noreply.github.com>
2023-10-24 13:23:37 -0500

    Project-0

---
+++ index.js
@@ -0,0 +1,28 @@
+let agregar = document.getElementById("agregar")
+let eliminar = document.getElementById("eliminar")
+let modificar = document.getElementById("modificar")
+let cantUni = document.getElementById("cantUni")
+let pago = document.getElementById("pago")
+
+let CantidadUnidad
+
+agregar.onclick = function(even){
+    alert(`casa`)
+    even.preventDefault
+}
+eliminar.onclick = function(even){
+    alert(`casa`)
+    even.preventDefault
+}
+modificar.onclick = function(even){
+    alert(`casa`)
+    even.preventDefault
+}
+cantUni.onclick = function(even){
+    alert(`casa`)
+    even.preventDefault
+}
+pago.onclick = function(even){
+    alert(`casa`)
+    even.preventDefault
+}
